594f3a8f66012542ddf0e8160f3fef732d51a6da
BayastanUrmatovich <boavlnooruz@gmail.com>
2022-05-23 15:24:05 +0600

    footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="Footer">
+      <div className="container">
+        Anime store karakol 2022
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
