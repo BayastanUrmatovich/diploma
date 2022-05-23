fc0c4eeb84cdaca8877bce6cb7f5909d6d126259
BayastanUrmatovich <boavlnooruz@gmail.com>
2022-05-23 15:25:42 +0600

    add categories

---
+++ src/data/caregories.js
@@ -0,0 +1,18 @@
+// import beansImage from "../assets/beans.jpeg";
+// import accessoriesImage from "../assets/accessories.jpeg";
+// import teaImage from "../assets/tea.webp";
+
+// const categories = [
+//   {
+// @@ -8,6 +9,12 @@ const categories = [
+//     image: animeImage,
+//     desciption: "A coffee bean is a seed of the Coffea plant and the source for coffee. It is the pip inside the red or purple fruit often referred to as a cherry."
+//   },
+//   {
+//     categoryId: "tea",
+//     title: "Specialty tea",
+//     image: teaImage,
+//     desciption: "Tea is an aromatic beverage prepared by pouring hot or boiling water over cured or fresh leaves of Camellia sinensis, an evergreen shrub native to China, India and other East Asian countries."
+//   },
+  
+//     categoryId: "accessories",
